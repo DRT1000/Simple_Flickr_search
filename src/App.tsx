4bf83ac2a805fc49ea70665ce926189aed00dbaf
DRT1000 <drt10000@gmail.com>
2021-05-29 18:23:31 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import style from './App.module.scss'
+
+export const App = () => {
+
+       return (
+        <div className={style.rect}>
+            RRRR
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: "./index.tsx",
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 3001,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./index.html"}),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new HtmlMinimizerPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: "css-loader",
+                    options: {
+                        importLoaders: 1,
+                        modules: true
+                    }
+                }, "sass-loader"],
+                include: /\.module\.s?css$/
+            },
+            {
+                test: /\.bundle\.ts$/,
+                use: {
+                    loader: 'bundle-loader',
+                    options: {
+                        name: '[name]'
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(svg|png|gif|jpg)$/,
+                exclude: /fonts/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(ttf|eot|woff|svg|woff2)$/,
+                loader: "file-loader"
+            }
+        ],
+    },
+};
